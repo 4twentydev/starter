9c2903f47f41e48b08e5c4e2b329c0ebcba0e306
4twentydev <4twenty.dev@gmail.com>
2025-02-03 18:42:40 -0700

    github button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Flex, Text, Button } from "@radix-ui/themes";
+import { Flex, Button } from "@radix-ui/themes";
 
 function App() {
   return (
 <>
-<Flex direction="column" gap="2">
-			<Text>Hello from Radix Themes :)</Text>
-			<Button>Lets go</Button>
+<Flex direction="column" gap="2" className="justify-center items-center h-screen"> 
+<Button className="fixed top-2 mx-auto"><a href="https://github.com/4twentydev" target='new'>
+    Github
+</a></Button>
 		</Flex>
 </>  );
 }
